ad477c0d3bd9e67b23d31889acd20bde23634b4a
Hermes Miraflor II <miraflor.hermes@microsoft.com>
2023-09-24 18:16:53 +0800

    First commit for the Azure Virtual Network resource based on the AVM specifications

---
+++ examples/all_default/variables.tf
@@ -0,0 +1,10 @@
+
+variable "vnet_location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "rg_location" {
+  type    = string
+  default = "eastus"
+}
---
+++ examples/new_route/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_location" {
+  type    = string
+  default = "westus"
+}
+
+variable "vnet_location" {
+  type    = string
+  default = "westus"
+}
