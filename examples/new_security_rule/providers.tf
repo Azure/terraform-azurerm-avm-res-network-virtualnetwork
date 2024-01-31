24b821518de324908f6a79fbe73cc3c051863d4d
Hermes Miraflor II <miraflor.hermes@microsoft.com>
2024-01-31 15:03:30 +0800

    fixed governance issues (#33)

--- examples/new_security_rule/providers.tf
+++ examples/new_security_rule/providers.tf
@@ -13,8 +13,8 @@ terraform {
 }
 provider "azurerm" {
   features {
-         resource_group {
-          prevent_deletion_if_contains_resources = false
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
 }
