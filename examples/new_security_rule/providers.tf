69e471b2dc11d8eb972e21605c2956ac41c7189b
Hermes Miraflor II <miraflor.hermes@microsoft.com>
2024-03-09 16:32:34 +0800

    synching files

--- examples/new_security_rule/providers.tf
+++ examples/new_security_rule/providers.tf
@@ -1,17 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=3.11.0, <4.0"
     }
-    curl = {
-      source  = "anschoewe/curl"
-      version = "1.0.2"
-    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
     }
+    curl = {
+      source  = "anschoewe/curl"
+      version = "1.0.2"
+    }
+    
   }
 }
 provider "azurerm" {
