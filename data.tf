fb970f575eb8a7b70a0fda83749bfe773412be4f
Chris Chapman <chris.chapman@bjss.com>
2024-11-24 12:18:51 +0000

    improve data sources and logic for dynamic subnet allocation

--- data.tf
+++ data.tf
@@ -1,1 +1,8 @@
 data "azurerm_client_config" "this" {}
+# When creating a vnet with IPAM - The addressPrefixes is not returned on subsequent refreshes of the resource as it
+# is not given when invoking the API. So we make a call to the API to get the addressPrefixes for use when creating the subnets.
+data "azapi_resource" "vnet" {
+  type      = "Microsoft.Network/virtualNetworks@2024-03-01"
+  name      = azapi_resource.vnet.name
+  parent_id = azapi_resource.vnet.parent_id
+}
--- locals.tf
+++ locals.tf
@@ -1,4 +1,16 @@
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  subscription_id                    = coalesce(var.subscription_id, data.azurerm_client_config.this.subscription_id)
+  # for each subnet locate the ones which require dynamic allocation of IP addresses and calculate the bit offset
+  subnet_newbits = { for key, value in var.subnets :
+    key => try(value.address_prefix_size, 32) - try(split("/", data.azapi_resource.vnet.output.properties.addressSpace.addressPrefixes[0])[1], 0) if value.address_prefix_size != null
+  }
+  #Generate the prefixes
+  subnet_prefixes = cidrsubnets(data.azapi_resource.vnet.output.properties.addressSpace.addressPrefixes[0], values(local.subnet_newbits)...)
+  subnets = { for key, value in var.subnets : key => merge(
+    value,
+    {
+      address_prefix = contains(keys(local.subnet_newbits), key) ? element(local.subnet_prefixes, index(keys(local.subnet_newbits), key)) : value.address_prefix,
+    }
+  ) }
+  subscription_id = coalesce(var.subscription_id, data.azurerm_client_config.this.subscription_id)
 }
