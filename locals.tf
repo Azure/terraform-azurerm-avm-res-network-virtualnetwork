10c5030c728673cf452c1dca59cbc5223bee9eb5
Haflidi Fridthjofsson <26624010+haflidif@users.noreply.github.com>
2025-10-06 19:21:07 +0200

    chore: remove unused subscription_id local variable

--- locals.tf
+++ locals.tf
@@ -2,5 +2,4 @@ data "azurerm_client_config" "current" {}
 
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
-  subscription_id                    = data.azurerm_client_config.current.subscription_id
 }
