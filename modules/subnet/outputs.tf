2612b11994a2b71a9fb2103b740d7b5879defb96
Jared Holgate <jaredholgate@microsoft.com>
2025-10-15 13:20:58 +0100

    fix ipam null support

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -5,20 +5,20 @@ output "address_prefixes" {
 
 output "application_gateway_ip_configuration_resource_id" {
   description = "The application gateway ip configurations resource id."
-  value       = try(azapi_resource.subnet.body.properties.applicationGatewayIPConfigurations.id, null)
+  value       = local.ipam_enabled ? try(azapi_resource.subnet_ipam[0].body.properties.applicationGatewayIPConfigurations.id, null) : try(azapi_resource.subnet[0].body.properties.applicationGatewayIPConfigurations.id, null)
 }
 
 output "name" {
   description = "The resource name of the subnet."
-  value       = azapi_resource.subnet.name
+  value       = local.ipam_enabled ? azapi_resource.subnet_ipam[0].name : azapi_resource.subnet[0].name
 }
 
 output "resource" {
   description = "All attributes of the subnet"
-  value       = azapi_resource.subnet
+  value       = local.ipam_enabled ? azapi_resource.subnet_ipam[0] : azapi_resource.subnet[0]
 }
 
 output "resource_id" {
   description = "The resource ID of the subnet."
-  value       = azapi_resource.subnet.id
+  value       = local.ipam_enabled ? azapi_resource.subnet_ipam[0].id : azapi_resource.subnet[0].id
 }
